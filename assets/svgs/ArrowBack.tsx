88a8a960d9d45af53e46e60b22cc2dd0d20a81b2
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-24 23:13:18 +0000

    feat: svg assets for the comment page

---
+++ assets/svgs/ArrowBack.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export const ArrowBack = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    width="18"
+    height="14"
+    viewBox="0 0 18 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.33333 1.16667L1.5 6.99995M1.5 6.99995L7.33333 12.8333M1.5 6.99995H16.5"
+      stroke="#current"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
