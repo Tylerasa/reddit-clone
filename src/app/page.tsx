b4df78f410b264fe699c1696148f5bca0275a5b7
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-24 23:16:48 +0000

    refactor: moving post to its separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { ChevronUp } from "assets/svgs/ChevronUp";
 import { unstable_noStore as noStore } from "next/cache";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import { SingleFeedPost } from "./_components/Posts/SingleFeedPost";
 
 export default async function Home() {
   noStore();
@@ -13,52 +14,24 @@ export default async function Home() {
         <div className="pt-10">
           <CreatePost />
         </div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
+        <SingleFeedPost />
       </div>
     </main>
   );
 }
-
-const Post = () => {
-  return (
-    <div className="flex w-full gap-4 border-b border-b-gray-200 py-10">
-      <div className="">
-        <div className="flex flex-col items-center gap-[10px] ">
-          <ChevronUp className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
-          <span className="font-medium text-gray-800">150</span>
-          <ChevronDown className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
-        </div>
-      </div>
-      <div className="flex flex-col gap-[6px]">
-        <div className="flex items-center gap-[6px]">
-          <div className="h-4 w-4 rounded-full bg-red-500"></div>
-          <p className="text-sm text-gray-600 ">
-            Posted by limerider 3 hours ago
-          </p>
-        </div>
-
-        <p className="font-medium ">Honest opinions on Lime ebikes in London</p>
-        <p className="text-sm leading-[20px]">
-          Tell me your good and bad experiences of using Lime as a Rider in
-          London
-        </p>
-      </div>
-    </div>
-  );
-};
 
 const CreatePost = () => {
   return (
