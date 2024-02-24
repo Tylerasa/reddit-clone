91fc1c0c3518f61b09c7c85c9cf4127b87a11f2b
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-24 20:32:20 +0000

    feat: layout for the index page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import "~/styles/globals.css";
+import "~/styles/app.css";
 
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-
+import Link from "next/link";
+import { Home } from "assets/svgs/Home";
+import { Login } from "assets/svgs/Login";
+import Image from "next/image";
+import person from "assets/images/person.png";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -22,7 +27,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body
+        className={`font-sans ${inter.variable} flex h-screen w-screen bg-white`}
+      >
+        <aside className="flex h-screen w-[17.313rem] flex-col overflow-y-auto border-r border-r-gray-200 p-4 font-medium text-gray-700">
+          <div className="flex flex-1 flex-col gap-1">
+            <Link
+              href={"/"}
+              className="hover:bg-nav-link-bg  nav-link flex w-full items-center gap-4 rounded-xl px-4 py-3"
+            >
+              <Home stroke="#4B5563" className="h-5 w-5" />
+              <span>Home</span>
+            </Link>
+            <Link
+              href={"/login"}
+              className="hover:bg-nav-link-bg  nav-link flex w-full items-center gap-4 rounded-xl px-4 py-3"
+            >
+              <Login stroke="#4B5563" />
+              <span>Log in</span>
+            </Link>
+          </div>
+          <div className="flex items-center gap-4 px-3 py-4">
+            <Image
+              src={person}
+              alt="picture of an person"
+              className="h-8 w-8"
+            />
+            <span>Christine Cook</span>
+          </div>
+        </aside>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,84 @@
+import { ChevronDown } from "assets/svgs/ChevronDown";
+import { ChevronUp } from "assets/svgs/ChevronUp";
 import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 export default async function Home() {
   noStore();
 
   return (
-   <main></main>
+    <main className="feed h-screen w-screen overflow-y-auto pb-10">
+      <div className=" w-[37.5rem]">
+        <div className="pt-10">
+          <CreatePost />
+        </div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
   );
 }
 
+const Post = () => {
+  return (
+    <div className="flex w-full gap-4 border-b border-b-gray-200 py-10">
+      <div className="">
+        <div className="flex flex-col items-center gap-[10px] ">
+          <ChevronUp className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
+          <span className="font-medium text-gray-800">150</span>
+          <ChevronDown className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-[6px]">
+        <div className="flex items-center gap-[6px]">
+          <div className="h-4 w-4 rounded-full bg-red-500"></div>
+          <p className="text-sm text-gray-600 ">
+            Posted by limerider 3 hours ago
+          </p>
+        </div>
+
+        <p className="font-medium ">Honest opinions on Lime ebikes in London</p>
+        <p className="text-sm leading-[20px]">
+          Tell me your good and bad experiences of using Lime as a Rider in
+          London
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const CreatePost = () => {
+  return (
+    <div className="shadow-card-shadow flex w-full gap-4 rounded-xl border  border-gray-200 bg-white px-3 py-4">
+      <div className="h-4 w-4 rounded-full bg-red-500"></div>
+      <div className="w-full">
+        <div className="flex w-full flex-col gap-3 border-b border-b-gray-200 pb-3">
+          <Input
+            placeholder="Title of your post"
+            className="h-6 w-full border-none p-0 placeholder:text-gray-500 focus-visible:ring-0 focus-visible:ring-offset-0"
+          />
+          <Input
+            placeholder="Share your thoughts with the world!"
+            className="h-6 w-full border-none p-0 placeholder:text-gray-500  focus-visible:ring-0 focus-visible:ring-offset-0"
+          />
+        </div>
+        <div className="pt-3 flex justify-end">
+          <Button className="bg-indigo-500 hover:bg-indigo-600">Post</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
