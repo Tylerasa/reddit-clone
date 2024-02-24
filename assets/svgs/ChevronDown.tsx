077ec76e72cb71018400487834fc0ce01567dbc6
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-24 20:31:18 +0000

    feat: svg assets for the index page

---
+++ assets/svgs/ChevronDown.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export const ChevronDown = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    width="16"
+    height="11"
+    viewBox="0 0 16 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.3333 0.83333L7.99992 4.16666L4.66659 0.833329M1.33325 3.33333L7.99992 10L14.6666 3.33333"
+      stroke="#current"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ assets/svgs/ChevronUp.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export const ChevronUp = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    width="16"
+    height="11"
+    viewBox="0 0 16 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.33325 7.66667L7.99992 1L14.6666 7.66667M4.66659 10.1665L7.99992 6.83318L11.3333 10.1665"
+      stroke="#current"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
