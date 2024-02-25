5a49e0ad7a855471655fccfdc1cdf36236a776b6
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-25 09:43:53 +0000

    refactor: moving the feed to it's separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,17 @@ import { SingleFeedPost } from "./_components/Posts/SingleFeedPost";
 import person from "assets/images/person.png";
 import Image from "next/image";
 import CheckAuthStatusFeed from "./_components/CheckAuthStatuFeed";
+import { RouterOutputs } from "~/trpc/shared";
+import Feed from "./_components/Shared/Feed";
 
 export default async function Home() {
   noStore();
 
   return (
     <main className="feed h-screen w-screen overflow-y-auto pb-10">
-      <div className=" w-[37.5rem]">
-          <CheckAuthStatusFeed/>
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
-        <SingleFeedPost />
+      <div className="w-[37.5rem]">
+        <CheckAuthStatusFeed />
+        <Feed />
       </div>
     </main>
   );
