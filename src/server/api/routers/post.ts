d5d8627df631abad7b13dcc4f750be1ca24cac35
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-25 15:17:17 +0000

    feat: creating posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,8 +1,13 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filteredUser = (user: User) => {
   return {
@@ -16,9 +21,8 @@ export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
-
-    
 
     const users = (
       await clerkClient.users.getUserList({
@@ -27,11 +31,48 @@ export const postRouter = createTRPCRouter({
       })
     ).map(filteredUser);
 
-    return posts.map((post) => ({
-      post,
-      author: users.find((user) => user.id === post.authorId),
-    }));
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+        });
+      }
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        title: z
+          .string()
+          .min(3, { message: "Title has to be more than 3 characters" })
+          .max(100, { message: "Title has to be less than 100 characters" }),
+        content: z
+          .string()
+          .min(3, { message: "Title has to be more than 3 characters" })
+          .max(280, { message: "Title has to be less than 280 characters" }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+      const { title, content } = input;
+      const post = await ctx.db.post.create({
+        data: {
+          authorId,
+          title,
+          content,
+        },
+      });
+      return post;
+    }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
