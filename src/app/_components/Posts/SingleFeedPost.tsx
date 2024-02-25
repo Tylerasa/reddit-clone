df61c1b9787c28da648c65783d9e131106b238b1
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-25 15:18:45 +0000

    feat: handling relative time

--- src/app/_components/Posts/SingleFeedPost.tsx
+++ src/app/_components/Posts/SingleFeedPost.tsx
@@ -3,7 +3,11 @@ import { ChevronUp } from "assets/svgs/ChevronUp";
 import Image from "next/image";
 import person from "assets/images/person.png";
 import { RouterOutputs } from "~/trpc/shared";
-import { useUser } from "@clerk/nextjs";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Skeleton } from "~/components/ui/skeleton";
+
+dayjs.extend(relativeTime);
 
 type PostWithUser = RouterOutputs["post"]["getAll"][number];
 
@@ -42,12 +46,40 @@ export const SingleFeedPost = (props: PostWithUser) => {
           )}
 
           <p className="text-sm lowercase text-gray-600">
-            Posted by {author?.username} 3 hours ago
+            Posted by {author?.username} {dayjs(post.createdAt).fromNow()}
           </p>
         </div>
 
         <p className="font-medium ">{post.title}</p>
         <p className="text-sm leading-[20px]">{post.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export const SkeletonSingleFeedPost = () => {
+  return (
+    <div className="flex w-full gap-4 border-b border-b-gray-200 py-10">
+      <div className="">
+        <div className="flex flex-col items-center gap-[10px] ">
+          <Skeleton className="h-4 w-4" />
+          <Skeleton className="h-6 w-7" />
+
+          <Skeleton className="h-4 w-4" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-[6px] w-full">
+        <div className="flex items-center gap-[6px]">
+          <Skeleton className="h-6 w-6 rounded-full" />
+          <Skeleton className="h-4 w-[211px]" />
+        </div>
+
+        <Skeleton className="h-5 w-[322px] " />
+        <div className="">
+          <Skeleton className="h-4 w-full " />
+          <Skeleton className="h-4 w-full my-1 " />
+          <Skeleton className="h-4 w-[80%]" />
+        </div>
       </div>
     </div>
   );
