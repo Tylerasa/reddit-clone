d93cc600c59668981166320c6bd482698b03b9ba
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-26 15:00:43 +0000

    feat: displaying the signout button on login

--- src/app/_components/Shared/SideNav.tsx
+++ src/app/_components/Shared/SideNav.tsx
@@ -3,56 +3,53 @@ import React from "react";
 import Link from "next/link";
 import { Home } from "assets/svgs/Home";
 import Image from "next/image";
-import person from "assets/images/person.png";
 
 import { SignOutButton, useUser } from "@clerk/nextjs";
 import CheckAuthStatusNav from "../CheckAuthStatusNav";
+import { Login } from "assets/svgs/Login";
 
 const SideNav = () => {
   const { user } = useUser();
-  console.log("user", user);
 
   return (
     <aside className="flex h-screen w-[17.313rem] flex-col overflow-y-auto border-r border-r-gray-200 p-4 font-medium text-gray-700">
       <div className="flex flex-1 flex-col gap-1">
         <Link
           href={"/"}
-          className="hov-but  flex w-full items-center gap-4 rounded-xl px-4 py-3 hover:bg-gray-50"
+          className={`hov-but  flex w-full items-center gap-4 rounded-xl bg-gray-50 px-4 py-3 text-indigo-500 hover:bg-gray-50`}
         >
-          <Home stroke="#4B5563" className="h-5 w-5" />
+          <Home className="h-5 w-5 stroke-indigo-600" />
           <span>Home</span>
         </Link>
         <CheckAuthStatusNav />
       </div>
-      <SignOutButton />
-      <div className="flex items-center gap-4 px-4 py-3">
+      <div className="">
         {user && user.hasImage ? (
-          <>
-            <Image
-              src={user.imageUrl}
-              alt={`picture of ${user.firstName}`}
-              className="h-8 w-8 rounded-full"
-              width={32}
-              height={32}
-            />
-            <div className="overflow-hidden">
-              <p className="word-clip">
-                {user.firstName} {user.lastName}
-              </p>
+          <div className="">
+            <SignOutButton>
+              <div className="hov-but  flex w-full cursor-pointer items-center gap-4 rounded-xl bg-gray-50 px-4  py-3 hover:bg-gray-50 ">
+                <Login className="h-5 w-5 rotate-180 stroke-gray-600 hover:stroke-indigo-600 " />
+                <span>Sign Out</span>
+              </div>
+            </SignOutButton>
+            <div className="flex items-center gap-4 px-4 py-3">
+              <>
+                <Image
+                  src={user.imageUrl}
+                  alt={`picture of ${user.firstName}`}
+                  className="h-8 w-8 rounded-full"
+                  width={32}
+                  height={32}
+                />
+                <div className="overflow-hidden">
+                  <p className="word-clip">
+                    {user.firstName} {user.lastName}
+                  </p>
+                </div>
+              </>
             </div>
-          </>
-        ) : (
-          <>
-            <Image
-              src={person}
-              alt="picture of an person"
-              className="h-8 w-8"
-              width={32}
-              height={32}
-            />
-            <span>Christine Cook</span>
-          </>
-        )}
+          </div>
+        ) : null}
       </div>
     </aside>
   );
