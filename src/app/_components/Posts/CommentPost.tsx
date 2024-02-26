b71c17ea94c8ee3e3fbfb983c8f48b9de44e36bf
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-26 04:19:53 +0000

    feat: comment post integrated

--- src/app/_components/Posts/CommentPost.tsx
+++ src/app/_components/Posts/CommentPost.tsx
@@ -6,43 +6,55 @@ import { Reply } from "assets/svgs/Reply";
 import Image from "next/image";
 import { useRef, useState } from "react";
 import ReplyPost from "./ReplyPost";
+import { Skeleton } from "~/components/ui/skeleton";
+import { RouterOutputs } from "~/trpc/shared";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-export const CommentPost = () => {
+dayjs.extend(relativeTime);
+
+type Comment = RouterOutputs["post"]["getComments"][number];
+
+export const CommentPost = (props: Comment) => {
+  const { comment, author } = props;
+
   const [showReplyForm, setShowReplyForm] = useState(false);
-  //   const ref = useRef(null);
-  //   const click = () => {
-  //     if (ref && ref.current) {
-  //       console.log("ref", ref.current);
-  //     }
-  //   };
+
   return (
-    <div
-      // ref={ref}
-      className=" pb-6"
-    >
+    <div className=" pb-6">
       <div className="flex w-full gap-4">
         <div className="flex flex-col gap-[6px]">
           <div className="flex items-center gap-[6px]">
-            <Image
-              src={person}
-              alt="picture of an person"
-              className="h-6 w-6"
-            />
-            <p className="text-sm text-gray-600 ">
-              Posted by limerider 3 hours ago
+            {author && author.imageUrl ? (
+              <Image
+                src={author.imageUrl}
+                alt={`picture of ${author.username}`}
+                className="h-6 w-6 rounded-full"
+                width={24}
+                height={24}
+              />
+            ) : (
+              <Image
+                src={person}
+                alt="picture of an person"
+                className="h-6 w-6"
+                width={24}
+                height={24}
+              />
+            )}
+            <p className="text-sm  text-gray-600">
+              Posted by <span className="lowercase">{author?.username}</span>{" "}
+              {dayjs(comment.createdAt).fromNow()}
             </p>
           </div>
 
-          <p className="text-sm leading-[20px] text-gray-800">
-            Lack of a text feedback box to describe an issue with a bike. I once
-            had a bicycle that had a steering lockup when trying to turn left.
-            Had to brake and stop otherwise would have driven straight onto
-            oncoming traffic.
-          </p>
+          <p className="text-sm leading-[20px] text-gray-800">{comment.text}</p>
 
           <div className="mt-3 flex items-center gap-2 text-sm text-gray-700">
             <ChevronUp className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
-            <span>6</span>
+            <span className="font-medium text-gray-800">
+              {comment.numUpvotes - comment.numDownvotes}
+            </span>
             <ChevronDown className="cursor-pointer stroke-gray-700 hover:stroke-indigo-600" />
             <div
               onClick={() => setShowReplyForm(!showReplyForm)}
@@ -62,9 +74,49 @@ export const CommentPost = () => {
       </div>
       {showReplyForm ? (
         <div className="mt-6">
-          <ReplyPost />
+          <ReplyPost
+            postId={comment.postId}
+            imageUrl={author.imageUrl}
+            username={author.username}
+          />
         </div>
       ) : null}
+    </div>
+  );
+};
+
+export const CommentPostSkeleton = () => {
+  return (
+    <div className="flex w-full gap-4 border-b border-b-gray-200 py-6">
+      {/* <div className="">
+        <div className="flex flex-col items-center gap-[10px] ">
+          <Skeleton className="h-4 w-4" />
+          <Skeleton className="h-6 w-7" />
+
+          <Skeleton className="h-4 w-4" />
+        </div>
+      </div> */}
+      <div className="flex w-full flex-col gap-[6px]">
+        <div className="flex items-center gap-[6px]">
+          <Skeleton className="h-6 w-6 rounded-full" />
+          <Skeleton className="h-4 w-[211px]" />
+        </div>
+
+        <div className="">
+          <Skeleton className="h-4 w-full " />
+          <Skeleton className="my-1 h-4 w-full " />
+          <Skeleton className="h-4 w-[80%]" />
+          <div className="mt-1 flex justify-start">
+            <div className="flex  items-center gap-[10px] ">
+              <Skeleton className="h-4 w-4" />
+              <Skeleton className="h-5 w-[14px]" />
+              <Skeleton className="h-4 w-4" />
+              <Skeleton className="h-4 w-4" />
+              <Skeleton className="h-4 w-[38px]" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
