b2e607225c7eddc841f9979ac1139b5f9dd5321c
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-26 04:17:49 +0000

    feat: page moved under id

--- src/app/r/[username]/comments/[id]/[title]/page.tsx
+++ src/app/r/[username]/comments/[id]/[title]/page.tsx
@@ -1,8 +1,9 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { ArrowBack } from "assets/svgs/ArrowBack";
-import { MainPost } from "~/app/_components/Posts/MainPost";
 import { CommentPost } from "~/app/_components/Posts/CommentPost";
 import Link from "next/link";
+import MainPost from "~/app/_components/Posts/MainPost";
+import CommentFeed from "~/app/_components/Comments/CommentFeed";
 
 export default async function PostPage() {
   noStore();
@@ -11,19 +12,19 @@ export default async function PostPage() {
     <main className="feed h-screen w-screen overflow-y-auto pb-10">
       <div className=" w-[37.5rem]">
         <div className="pt-6">
-          <Link href='/' role="button" className="flex cursor-pointer items-center gap-4">
+          <Link
+            href="/"
+            role="button"
+            className="flex cursor-pointer items-center gap-4"
+          >
             <ArrowBack className="stroke-gray-800" />
             <p className="text-sm font-medium text-gray-800">Back to posts</p>
           </Link>
-          <MainPost/>
-          
-          <div className="pt-6">
-            <h3 className="font-medium text-sm pb-6">All comments</h3>
-            <CommentPost/>
-            <CommentPost/>
-            <CommentPost/>
-            <CommentPost/>
+          <MainPost />
 
+          <div className="pt-6">
+            <h3 className="pb-6 text-sm font-medium">All comments</h3>
+            <CommentFeed />
           </div>
         </div>
       </div>
