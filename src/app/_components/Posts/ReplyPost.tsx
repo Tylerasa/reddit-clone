0e9b4c09a115e55eb90ef78d1e94be6624400ca9
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-26 04:18:47 +0000

    feat: ReplyPost scaffoling done

--- src/app/_components/Posts/ReplyPost.tsx
+++ src/app/_components/Posts/ReplyPost.tsx
@@ -1,26 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import person from "assets/images/person.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
 
-const ReplyPost = () => {
+const ReplyPost = (props: {
+  postId: number;
+  imageUrl: string;
+  username: string | null;
+}) => {
+  const { postId, imageUrl, username } = props;
+  const [text, setText] = useState("");
+  const router = useRouter();
+
+  const comment = api.post.comment.useMutation({
+    onSuccess: () => {
+      router.refresh();
+      setText("");
+    },
+  });
   return (
-    <div className="shadow-card-shadow flex w-full gap-4 rounded-xl border  border-gray-200 bg-white px-3 py-4">
-      <Image src={person} alt="picture of an person" className="h-6 w-6" />
-      <div className="w-full">
+    <div className="flex w-full gap-4 rounded-xl border border-gray-200  bg-white px-3 py-4 shadow-card-shadow">
+      <Image
+        src={imageUrl}
+        alt={`picture of ${username ?? ""}`}
+        className="h-6 w-6 rounded-full"
+        width={24}
+        height={24}
+      />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          comment.mutate({ text, postId });
+        }}
+        className="w-full"
+      >
         <div className="flex w-full flex-col gap-3 border-b border-b-gray-200 pb-3">
           <Input
+            onChange={(e) => setText(e.target.value)}
+            value={text}
             placeholder="Share your thoughts with the world!"
             className="h-6 w-full border-none p-0 placeholder:text-gray-500  focus-visible:ring-0 focus-visible:ring-offset-0"
           />
         </div>
         <div className="flex justify-end pt-3">
-          <Button className="rounded-[10px] bg-indigo-500 px-3 py-2 hover:bg-indigo-600">
+          <Button
+            disabled={comment.isLoading}
+            className="bg-indigo-500 hover:bg-indigo-600 disabled:bg-indigo-800/20"
+          >
             Comment
           </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
