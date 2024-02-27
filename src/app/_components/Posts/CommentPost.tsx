a18ef08a94647c9ee741d810ba597c5f762dc168
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-27 07:53:56 +0000

    feat: optimistic update for better UX

--- src/app/_components/Posts/CommentPost.tsx
+++ src/app/_components/Posts/CommentPost.tsx
@@ -4,7 +4,7 @@ import { ChevronDown } from "assets/svgs/ChevronDown";
 import { ChevronUp } from "assets/svgs/ChevronUp";
 import { Reply } from "assets/svgs/Reply";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReplyPost from "./ReplyPost";
 import { Skeleton } from "~/components/ui/skeleton";
 import { RouterOutputs } from "~/trpc/shared";
@@ -29,7 +29,16 @@ type Reply =
 export const CommentPost = (props: Comment) => {
   const { comment, author } = props;
 
+  const hasVoted = comment!.votes.find(
+    (vote: { authorId: any }) => vote.authorId === author.id,
+  );
+
   const [showReplyForm, setShowReplyForm] = useState(false);
+  const [commentState, setCommentState] = useState(comment);
+  const [optHasVoted, setOptHasVoted] = useState<number | null>(
+    hasVoted?.value ?? null,
+  );
+
   const ref = useRef<HTMLButtonElement>(null);
   const router = useRouter();
 
@@ -57,7 +66,73 @@ export const CommentPost = (props: Comment) => {
     },
   });
 
+  useEffect(() => {
+    setCommentState(comment);
+  }, [comment]);
+
   const handleVote = (value: number) => {
+    let newComment = { ...commentState };
+
+    // Optimistically update UI
+    if (optHasVoted === value) {
+      if (value === 1) {
+        newComment.numUpvotes -= 1;
+        let existingVoteIndex = newComment.votes.findIndex(
+          (v) => v.value === 1 && v.authorId === author.id,
+        );
+        if (existingVoteIndex !== -1) {
+          newComment.votes.splice(existingVoteIndex, 1);
+        }
+      } else {
+        newComment.numDownvotes -= 1;
+        let existingVoteIndex = newComment.votes.findIndex(
+          (v) => v.value === -1 && v.authorId === author.id,
+        );
+        if (existingVoteIndex !== -1) {
+          newComment.votes.splice(existingVoteIndex, 1);
+        }
+      }
+      setOptHasVoted(null);
+    } else {
+      if (value === 1) {
+        let existingVoteIndex = newComment.votes.findIndex(
+          (v) => v.value === -1 && v.authorId === author.id,
+        );
+        if (existingVoteIndex !== -1) {
+          newComment.numDownvotes -= 1;
+          newComment.votes.splice(existingVoteIndex, 1);
+        }
+
+        newComment.numUpvotes += 1;
+        newComment.votes.push({
+          id: Math.random(),
+          commentId: Math.random(),
+          postId: Math.random(),
+          authorId: author.id,
+          value: 1,
+        });
+      } else {
+        let existingVoteIndex = newComment.votes.findIndex(
+          (v) => v.value === 1 && v.authorId === author.id,
+        );
+        if (existingVoteIndex !== -1) {
+          newComment.numUpvotes -= 1;
+          newComment.votes.splice(existingVoteIndex, 1);
+        }
+        newComment.numDownvotes += 1;
+        newComment.votes.push({
+          id: Math.random(),
+          commentId: Math.random(),
+          postId: Math.random(),
+          authorId: author.id,
+          value: -1,
+        });
+      }
+      setOptHasVoted(value);
+    }
+
+    setCommentState(newComment);
+
     if (hasVoted?.value === value) {
       // Remove vote
       if (value === 1) {
@@ -75,12 +150,8 @@ export const CommentPost = (props: Comment) => {
     }
   };
 
-  const hasVoted = comment!.votes.find(
-    (vote: { authorId: any }) => vote.authorId === author.id,
-  );
-
   const handleReplyClick = () => {
-setShowReplyForm(!showReplyForm);
+    setShowReplyForm(!showReplyForm);
 
     if (ref && ref.current) {
       ref.current.click();
@@ -88,7 +159,7 @@ setShowReplyForm(!showReplyForm);
   };
 
   return (
-    <Accordion type="single" collapsible >
+    <Accordion type="single" collapsible>
       <AccordionItem value={`item-${comment.id}`} className="">
         <div className="pb-4">
           <div className="flex w-full gap-4 border-b pb-4">
@@ -116,12 +187,12 @@ setShowReplyForm(!showReplyForm);
                   <p className="text-sm  text-gray-600">
                     Posted by{" "}
                     <span className="lowercase">{author?.username}</span>{" "}
-                    {dayjs(comment.createdAt).fromNow()}
+                    {dayjs(commentState.createdAt).fromNow()}
                   </p>
                 </div>
 
                 <p className="text-left text-sm leading-[20px] text-gray-800">
-                  {comment.text}
+                  {commentState.text}
                 </p>
               </div>
 
@@ -129,16 +200,16 @@ setShowReplyForm(!showReplyForm);
                 <ChevronUp
                   onClick={() => handleVote(1)}
                   className={`: cursor-pointer hover:stroke-indigo-600
-          ${hasVoted?.value === 1 ? "stroke-indigo-600 " : "stroke-gray-700"}
+          ${optHasVoted === 1 ? "stroke-indigo-600 " : "stroke-gray-700"}
           `}
                 />
                 <span className="font-medium text-gray-800">
-                  {comment.numUpvotes - comment.numDownvotes}
+                  {commentState.numUpvotes - commentState.numDownvotes}
                 </span>
                 <ChevronDown
                   onClick={() => handleVote(-1)}
                   className={`: cursor-pointer hover:stroke-indigo-600
-          ${hasVoted?.value === -1 ? "stroke-indigo-600 " : "stroke-gray-700"}
+          ${optHasVoted === -1 ? "stroke-indigo-600 " : "stroke-gray-700"}
           `}
                 />
                 <div
@@ -161,17 +232,17 @@ setShowReplyForm(!showReplyForm);
             {showReplyForm ? (
               <div className="mt-6">
                 <ReplyPost
-                  commentId={comment.id}
-                  postId={comment.postId}
+                  commentId={commentState.id}
+                  postId={commentState.postId}
                   imageUrl={author.imageUrl}
                   username={author.username}
                 />
               </div>
             ) : null}
 
-            {comment.replies.length > 0
-              ? comment.replies.map((reply) => <ReplyComment {...reply} />)
-              : null}
+            {commentState.replies.map((reply) => (
+              <ReplyComment key={reply.id} {...reply} />
+            ))}
           </AccordionContent>
         </div>
       </AccordionItem>
